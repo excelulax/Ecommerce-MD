6a3a1dc4f9cc3f593977424eb2092e1571369412
Yanet Colque <celta.loxcel@gmail.com>
2025-01-19 23:14:03 -0400

    add identity service and cotroller

--- Shipping/Shipping.Infrastructure/Gateway/IdentityService.cs
+++ Shipping/Shipping.Infrastructure/Gateway/IdentityService.cs
@@ -1,7 +1,37 @@
-﻿namespace Shipping.Infrastructure.Gateway
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Shipping.Infrastructure.Gateway
 {
     public class IdentityService
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+        string url = "http://localhost:5238/api/Auth/Login";
+        public async Task<Response?> PostRequestAsync(Request request)
+        {
+            try
+            {
+                var jsonContent = JsonSerializer.Serialize(request);
+                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
+                var response = await httpClient.PostAsync(url, httpContent);
+
+                response.EnsureSuccessStatusCode();
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                var result = JsonSerializer.Deserialize<Response>(jsonResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during POST: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
